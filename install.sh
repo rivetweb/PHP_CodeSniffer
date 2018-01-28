fc0d5f88ba0998801c331e5ac5855ffc1b04de40
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2018-01-28 11:32:59 +0300

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 
 curl -OL https://github.com/wimg/PHPCompatibility/archive/8.1.0.zip
+rm -R PHPCompatibility-8.1.0
 unzip 8.1.0.zip
 rm 8.1.0.zip
-#rm -R PHPCompatibility
-#mv PHPCompatibility-8.1.0 PHPCompatibility
-# phpcs --config-set installed_paths ~/bin/PHPCompatibility
+
+rm -R CodeSniffer/Standards/PHPCompatibility
+rm CodeSniffer/Standards/PHPCSAliases.php
+
+mv PHPCompatibility-8.1.0/PHPCompatibility/ CodeSniffer/Standards/PHPCompatibility/
+mv PHPCompatibility-8.1.0/PHPCSAliases.php CodeSniffer/Standards/PHPCSAliases.php
+
+rm -R PHPCompatibility-8.1.0
+
+#phpcs --config-set installed_paths ~/bin/PHPCompatibility
